9280abd038996c8dbbe295c4538633918987b714
Marcela Nascimento Silva <marcelamns.dev@gmail.com>
2022-12-06 20:12:48 -0300

    first commit

---
+++ src/pages/App.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import gitLogo from "../assets/github.png";
+import { Container } from "./styles";
+import Input from "../components/Input";
+import ItemRepo from "../components/ItemRepo";
+import Button from "../components/Button";
+import { api } from "../services/api";
+
+function App() {
+  const [currentRepo, setCurrentRepo] = useState("");
+  const [repos, setRepos] = useState([]);
+
+  const handleSearchRepo = async () => {
+    const { data } = await api.get(`repos/${currentRepo}`);
+
+    if (data.id) {
+      const isExist = repos.find((repo) => repo.id === data.id);
+
+      if (!isExist) {
+        setRepos((prev) => [...prev, data]);
+        setCurrentRepo("");
+        return;
+      }
+    }
+    alert("Repositório não encontrado ou já adicionado!");
+  };
+
+  const handleRemoveRepo = (id) => {
+    const newRepos = repos.filter((repo) => {
+      return repo.id != id;
+    });
+    setRepos(newRepos);
+  };
+
+  return (
+    <Container>
+      <img src={gitLogo} width={72} height={72} alt="GitHub Logo" />
+      <Input
+        value={currentRepo}
+        onChange={(e) => setCurrentRepo(e.target.value)}
+      />
+      <Button onClick={handleSearchRepo} />
+      {repos.map((repo) => (
+        <ItemRepo handleRemoveRepo={handleRemoveRepo} repo={repo} />
+      ))}
+    </Container>
+  );
+}
+
+export default App;
